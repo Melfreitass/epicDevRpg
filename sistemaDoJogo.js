181418b7b3ff8bdfec653f3ddfb3c918027c9729
Melfreitass <melissa.g.freitas@aluno.senai.br>
2026-02-26 17:09:02 -0300

    first commit

---
+++ sistemaDoJogo.js
@@ -0,0 +1,13 @@
+class Personagem {
+    #HP;
+
+     constructor(nome, HP, poderDeAtaque) {
+        if (typeof HP !== 'number' || HP <=0) {
+            throw new Error('HP não pode ser negativo')
+        }
+
+        this.nome = nome;
+        this.#HP = HP;
+        this.poderDeAtaque = poderDeAtaque;
+     }
+}
